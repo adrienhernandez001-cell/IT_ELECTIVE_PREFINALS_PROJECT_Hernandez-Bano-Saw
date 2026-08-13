583ea1d239c1995aff6da67222d6bfcbc2f39eb3
adrienhernandez001-cell <jadehindot@gmail.com>
2026-08-13 20:09:40 +0800

    Add departments page

---
+++ IT_ELECTIVE_PREFINALS_PROJECT/Controllers/DepartmentsController.cs
@@ -0,0 +1,25 @@
+﻿using IT_ELECTIVE_PREFINALS_PROJECT.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly HelpDeskContext _context;
+
+        public DepartmentsController(HelpDeskContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var departments = await _context.Departments
+                .Include(d => d.Employees)
+                .ToListAsync();
+
+            return View(departments);
+        }
+    }
+}
